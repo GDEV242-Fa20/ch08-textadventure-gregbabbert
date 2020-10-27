f840c4b090793373cedfe45cdebe4980bc155310
gregbabbert <60162794+gregbabbert@users.noreply.github.com>
2020-10-27 14:32:24 -0400

    Create Main class
    This is for playing game outside of BlueJ

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class contains the main method which can be used to play
+ * the game outside of BlueJ.
+ *
+ * @author Greg Babbert
+ * @version October 27, 2020
+ */
+public class Main
+{
+    
+     /**
+      * The main method is used to start the game.
+      */
+     public static void main(String[] args) {
+         Game game = new Game();
+         game.play();
+     }   
+}
